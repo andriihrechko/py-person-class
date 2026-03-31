7fede6b3414b6c048bbaa9d0d12b26357a377193
andriihrechko <andriihrechko250703@gmail.com>
2026-03-31 11:41:50 +0300

    Solution Final

--- app/main.py
+++ app/main.py
@@ -8,15 +8,12 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    people_list = []
-    for person in people:
-        Person(person["name"], person["age"])
+    people_list = [Person(person["name"], person["age"]) for person in people]
 
     for person in people:
         current_person = Person.people[person["name"]]
         for relation in ("wife", "husband"):
-            if relation in person and person[relation] is not None:
+            if person.get(relation) and person[relation] is not None:
                 partner = person[relation]
                 setattr(current_person, relation, Person.people[partner])
-        people_list.append(current_person)
     return people_list
