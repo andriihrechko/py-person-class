80dfe1d2cd9b273188627abf6ce9b10f321933b9
andriihrechko <andriihrechko250703@gmail.com>
2026-03-31 12:10:01 +0300

    Fixed Solution

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ def create_person_list(people: list) -> list:
     for person in people:
         current_person = Person.people[person["name"]]
         for relation in ("wife", "husband"):
-            if person.get(relation) and person[relation] is not None:
+            if person.get(relation):
                 partner = person[relation]
                 setattr(current_person, relation, Person.people[partner])
     return people_list
